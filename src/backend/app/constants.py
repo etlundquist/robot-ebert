910ca7530fb77e23cbf35cffe8b022bf08405dbd
Eric Lundquist <lundquist.eric@bcg.com>
2023-11-26 15:34:13 -0800

    working llamaindex retriever

--- src/backend/app/constants.py
+++ src/backend/app/constants.py
@@ -5,22 +5,34 @@ import chromadb
 from dotenv import load_dotenv
 from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 
+from llama_index import ServiceContext
+from llama_index.llms import OpenAI
+from llama_index.embeddings import OpenAIEmbedding
+from llama_index.vector_stores import ChromaVectorStore
+from llama_index.indices.vector_store import VectorStoreIndex
+
 from backend.app.database import get_prod_engine
 
 
 QUERY_SCORE_WEIGHT = 0.9
+SIMILARITY_TOP_K = 10
 
 load_dotenv()
 
-# create LLM and VectorDB clients
+engine = get_prod_engine()
+
 openai_client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 chroma_client = chromadb.PersistentClient(path="./chroma")
 
-# create collection-specific VectorDB clients
 embedding_function = OpenAIEmbeddingFunction(api_key=os.environ["OPENAI_API_KEY"], model_name="text-embedding-ada-002")
-movies_content_index = chroma_client.get_collection(name="movies-content", embedding_function=embedding_function)
-users_collab_index = chroma_client.get_collection(name="users-collab", embedding_function=embedding_function)
-movies_collab_index = chroma_client.get_collection(name="movies-collab", embedding_function=embedding_function)
+movies_content_collection = chroma_client.get_collection(name="movies-content", embedding_function=embedding_function)
+users_collab_collection = chroma_client.get_collection(name="users-collab", embedding_function=embedding_function)
+movies_collab_collection = chroma_client.get_collection(name="movies-collab", embedding_function=embedding_function)
 
-# create SQLDB engine
-engine = get_prod_engine()
+llm = OpenAI(model="gpt-4-1106-preview", temperature=0.1, max_tokens=1000, api_key=os.environ["OPENAI_API_KEY"])
+embed_model = OpenAIEmbedding(model="text-embedding-ada-002", api_key=os.environ["OPENAI_API_KEY"])
+service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
+
+movies_content_vector_store = ChromaVectorStore(chroma_collection=movies_content_collection)
+movies_content_vector_index = VectorStoreIndex.from_vector_store(vector_store=movies_content_vector_store, service_context=service_context)
+movies_content_retriever = movies_content_vector_index.as_retriever(similarity_top_k=SIMILARITY_TOP_K)
--- src/backend/app/lib.py
+++ src/backend/app/lib.py
@@ -1,4 +1,3 @@
-import openai
 import numpy as np
 import pandas as pd
 
@@ -7,12 +6,12 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sqlalchemy import select
 
 from backend.app import database
-from backend.app.constants import engine, openai_client, movies_content_index, users_collab_index, movies_collab_index, QUERY_SCORE_WEIGHT
+from backend.app.constants import engine, openai_client, users_collab_collection, movies_collab_collection, movies_content_retriever, QUERY_SCORE_WEIGHT
 from shared.models import Movie, Recommendation
 
 
 def embed_query(query: str) -> List[float]:
-    """convert a natural language query into an embedding vector"""
+    """encode a natural language query into an embedding vector"""
 
     response = openai_client.embeddings.create(input=query, model="text-embedding-ada-002")
     embedding = response.data[0].embedding
@@ -20,7 +19,7 @@ def embed_query(query: str) -> List[float]:
 
 
 def get_movies(tmdb_ids: List[str]) -> List[Movie]:
-    """get a list of movies by ID sorted by ID"""
+    """get a list of Movie objects sorted by ID"""
 
     with engine.begin() as cnx:
         statement = select(database.movies).where(database.movies.c.tmdb_id.in_(tmdb_ids)).order_by(database.movies.c.tmdb_id)
@@ -32,8 +31,8 @@ def get_user_recs(user_id: str, k: int = 10) -> List[Recommendation]:
     """get a list of movie recommendations based on a user's collaborative filtering embedding"""
 
     # find the best matches based on the user's collaborative filtering embedding
-    user_embedding = users_collab_index.get(ids=user_id, include=["embeddings"])["embeddings"][0]
-    user_matches = movies_collab_index.query(query_embeddings=user_embedding, n_results=k, include=["embeddings", "distances"])
+    user_embedding = users_collab_collection.get(ids=user_id, include=["embeddings"])["embeddings"][0]
+    user_matches = movies_collab_collection.query(query_embeddings=user_embedding, n_results=k, include=["embeddings", "distances"])
     user_matches = sorted(zip(user_matches["ids"][0], user_matches["distances"][0]), key=lambda x: x[0])
 
     # get sorted lists of [movies, scores] by tmdb_id
@@ -48,32 +47,34 @@ def get_user_recs(user_id: str, k: int = 10) -> List[Recommendation]:
 def get_search_recs(query: str, user_id: Optional[str] = None, k: int = 10) -> List[Recommendation]:
     """get a list of movie recommendations based on a user's search query embedding"""
 
-    # find the best matches based on the user's search query embedding
-    query_embedding = embed_query(query)
-    query_matches = movies_content_index.query(query_embeddings=query_embedding, n_results=k, include=["embeddings", "distances"])
-    query_matches = sorted(zip(query_matches["ids"][0], query_matches["distances"][0]), key=lambda x: x[0])
-    query_movie_scores = pd.Series(data=[1.0 - match[1] for match in query_matches], index=[match[0] for match in query_matches])
+    # find the best movie matches based on the user's query sorting the result by [tmdb_id]
+    query_matches = sorted(movies_content_retriever.retrieve(query), key=lambda x: x.node_id)
 
-    # get list of movies and a series of scores based on the query matches sorting the result by tmdb_id
-    query_movies = get_movies(tmdb_ids=[match[0] for match in query_matches])
+    # create an ordered list of movie IDs and a series of [id, score] pairs from the query matches
+    query_match_movies = [match.node_id for match in query_matches]
+    query_movie_scores = pd.Series(data=[match.score for match in query_matches], index=query_match_movies)
+
+    # get list of movies and a series of scores based on the query matches sorting the result by [tmdb_id]
+    query_movies = get_movies(tmdb_ids=query_match_movies)
 
     if user_id:
 
         # get the user's CF embedding and the CF embeddings for the query match movies sorted by tmdb_id
-        user_cf_embedding = users_collab_index.get(ids=user_id, include=["embeddings"])["embeddings"][0]
-        movie_cf_embeddings = movies_collab_index.get(ids=[match[0] for match in query_matches], include=["embeddings"])["embeddings"]
+        user_cf_embedding = users_collab_collection.get(ids=user_id, include=["embeddings"])["embeddings"][0]
+        movie_cf_embeddings = movies_collab_collection.get(ids=query_match_movies, include=["embeddings"])["embeddings"]
 
-        # calculate user-movie scores for the query match movies based on user-movie CF embedding similarities
+        # calculate user-movie scores for the query match movies based on user-movie CF embedding cosine similarity
         user_movie_scores = cosine_similarity(np.array(movie_cf_embeddings), np.array(user_cf_embedding).reshape(1, -1)).squeeze()
-        user_movie_scores = pd.Series(data=user_movie_scores, index=[match[0] for match in query_matches])
+        user_movie_scores = pd.Series(data=user_movie_scores, index=query_match_movies)
 
     else:
 
         # calculate user-movie scores for the query match movies based on movie popularity scaled onto [0, 1]
+        # FIXME: normalize the popularity scores wrt the entire database of movies not just the topK matches
         user_movie_scores = pd.Series(data=[movie.popularity for movie in query_movies], index=[movie.tmdb_id for movie in query_movies])
         user_movie_scores = (user_movie_scores - user_movie_scores.min()) / (user_movie_scores.max() - user_movie_scores.min())
 
-    # create re-ranked combined movie scores as a weighed average of the [query-movie] and [user-movie] scores sorting the result by tmdb_id
+    # re-rank the movie scores using a weighed average of the [query_movie] and [user_movie] scores sorting the result by [tmdb_id]
     combined_movie_scores = (QUERY_SCORE_WEIGHT * query_movie_scores + (1 - QUERY_SCORE_WEIGHT) * user_movie_scores).sort_index()
 
     # convert the [movie, score] pairs into recommendation objects and return sorted by descending score
